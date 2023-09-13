1f274ef67aa50b5d5dd11d2627cb736fec215e29
Chia1104 <yuyuchia7423@gmail.com>
2023-09-13 15:31:47 +0800

    test

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  // integrations: [react()],
   output: "server",
   // outDir: "build",
   adapter: node({
